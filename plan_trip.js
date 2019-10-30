3cb45581b098ef9224aee991515404577cbc986f
yangj132 <goldtrick@DESKTOP-0094F6F.localdomain>
2019-10-29 22:29:55 -0400

    Added new features to plan trip

--- plan_trip.js
+++ plan_trip.js
@@ -1,14 +1,31 @@
 const libraryBooks = []
-
+const HotTable = document.getElementById('HotPlaceTable');
+var num_row = 0;
+const row = HotTable.insertRow(num_row);
 function addNewHotTable(place,index) {
 
-    const HotTable = document.getElementById('HotPlaceTable');
-    const row = HotTable.insertRow(index);
-    const cell = row.insertCell(0);
+    /*const HotTable = document.getElementById('HotPlaceTable');
+    const row = HotTable.insertRow(index);*/
+    const cell = row.insertCell(index);
     const HotButton = document.createElement('button')
-    HotButton.className = 'HotButton'
+    HotButton.className = 'btn btn-outline-success my-2 my-sm-0'
+    HotButton.style.backgroundColor = 'purple'
+    HotButton.id = place
+
+    //HotButton.onclick = function(){changeBackground()}
+    //place.addEventListener('button',changeBackground)
     HotButton.appendChild(document.createTextNode(place))
     cell.appendChild(HotButton);
+    document.getElementById(place).onclick = function(){changeBackground(place)}
+    /*document.getElementsByClassName("HotButton").style.height = "100px";*/
 }
-addNewHotTable('New York',0)
-addNewHotTable('London',1)
+addNewHotTable('Santorini',0)
+addNewHotTable('Machu_Picchu',1)
+
+function changeBackground(ID) {
+    console.log(ID)
+    const background = ID + '.jpg'
+    document.getElementById("background_img").src = background
+    //document.body.style.backgroundImage = "background2.jpg";
+
+}
