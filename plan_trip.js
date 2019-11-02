5ff69b9b9fb6842e243d7db33d439bc75e8b541f
yangzhey <zheyu.yang@mail.utoronto.ca>
2019-11-02 04:44:21 -0400

    added plan trip and triplist

--- plan_trip.js
+++ plan_trip.js
@@ -1,31 +1,61 @@
 const libraryBooks = []
 const HotTable = document.getElementById('HotPlaceTable');
 var num_row = 0;
+var disPlayList = [];
 const row = HotTable.insertRow(num_row);
-function addNewHotTable(place,index) {
 
-    /*const HotTable = document.getElementById('HotPlaceTable');
-    const row = HotTable.insertRow(index);*/
+const PlaceInfoForm = document.querySelector('#PlaceInfoForm');
+PlaceInfoForm.addEventListener('submit', searchATrip);
+
+function addNewHotTable(place,index,text) {
     const cell = row.insertCell(index);
-    const HotButton = document.createElement('button')
-    HotButton.className = 'btn btn-outline-success my-2 my-sm-0'
-    HotButton.style.backgroundColor = 'purple'
-    HotButton.id = place
+    const Introduction = document.createElement('div');
+    const HotButton = document.createElement('button');
+    HotButton.className = 'btn btn-outline-success my-2 my-sm-0';
+    HotButton.style.backgroundColor = 'rgba(255,255,255,0.6)';
+    HotButton.style.color= 'black';
+    HotButton.id = place;
+    HotButton.appendChild(document.createTextNode(place));
+    cell.appendChild(Introduction);
+    const Description = document.createElement('div');
+    const DescriptionText = document.createTextNode(text);
+    Description.appendChild(DescriptionText);
 
-    //HotButton.onclick = function(){changeBackground()}
-    //place.addEventListener('button',changeBackground)
-    HotButton.appendChild(document.createTextNode(place))
-    cell.appendChild(HotButton);
+    Description.style.margin = '5%';
+
+    Introduction.appendChild(HotButton);
+    Introduction.appendChild(Description);
     document.getElementById(place).onclick = function(){changeBackground(place)}
-    /*document.getElementsByClassName("HotButton").style.height = "100px";*/
 }
-addNewHotTable('Santorini',0)
-addNewHotTable('Machu_Picchu',1)
-
+addNewHotTable('Santorini',0,"Santorini, officially Thira and classic Greek Thera, is an island in the southern Aegean Sea, about 200 km (120 mi) southeast of Greece's mainland. It is the largest island of a small, circular archipelago, which bears the same name and is the remnant of a volcanic caldera. It forms the southernmost member of the Cyclades group of islands, with an area of approximately 73 km2 (28 sq mi) and a 2011 census population of 15,550. The municipality of Santorini includes the inhabited islands of Santorini and Therasia, as well as the uninhabited islands of Nea Kameni, Palaia Kameni, Aspronisi and Christiana. The total land area is 90.623 km2 (34.990 sq mi).")
+addNewHotTable('Machu_Picchu',1,"Machu Picchu is a 15th-century Inca citadel, located in the Eastern Cordillera of southern Peru, on a 2,430-metre (7,970 ft) mountain ridge. It is located in the Cusco Region, Urubamba Province, Machupicchu District, above the Sacred Valley, which is 80 kilometres (50 mi) northwest of Cuzco and through which the Urubamba River flows, cutting through the Cordillera and creating a canyon with a tropical mountain climate.")
 function changeBackground(ID) {
-    console.log(ID)
-    const background = ID + '.jpg'
+    console.log(ID);
+    const background = 'pic/' + ID + '.jpg';
     document.getElementById("background_img").src = background
-    //document.body.style.backgroundImage = "background2.jpg";
+}
+function jumpTo(url) {
 
+        window.location.href = url;
+
+
+}
+function searchATrip(e) {
+    e.preventDefault();
+    disPlayList = [];
+    const InterestedPlaces = document.querySelector('#searchInterestedPlaces').value;
+    console.log(planList)
+    console.log(InterestedPlaces)
+    for (let i=0 ; i<planList.length; i++){
+        for (let j=0; j< planList[i].SpotList.length;j++){
+            console.log(planList[i].SpotList[j]);
+            if (planList[i].SpotList[j] == InterestedPlaces){
+                //addNewPlanToView(planList[i]);
+                console.log(planList[i]);
+                disPlayList.push(i);
+                console.log(planList[i].disPlay)
+                jumpTo('tripsList.html');
+            }
+        }
+    }
 }
---
+++ tripsList.js
@@ -0,0 +1,125 @@
+
+var planList = [];
+var numberOfPlans = 1;
+
+//
+
+
+class Plan {
+    constructor(StartPlace, EndPlace, MethodOfTravel,ExceptCost,StartDate,EndDate,Img,Author) {
+        this.StartPlace = StartPlace;
+        this.EndPlace = EndPlace;
+        this.MethodOfTravel = MethodOfTravel;
+        this.ExceptCost = ExceptCost;
+        this.StartDate = StartDate;
+        this.EndDate = EndDate;
+        this.SpotList = [];
+        this.PlanID = numberOfPlans;
+        this.Img = Img;
+        this.Author = Author;
+        this.numPeople = 1;
+        this.disPlay = false;
+        numberOfPlans++;
+
+    }
+}
+
+var Trip1 = new Plan('Athens','Santorini','bike',1000,'2019.11.01','2019.12.01','pic/Santorini.jpg','Tony');
+Trip1.SpotList.push('Athens');
+Trip1.SpotList.push('Santorini');
+
+function addNewPlan(Plan) {
+    planList.push(Plan);
+    //addNewPlanToView(Plan);
+}
+function addNewPlanToView(Plan){
+    const content = document.getElementById('pinBoot');
+    const plan = document.createElement('article');
+    plan.className = "white-panel";
+    plan.id = "white-panel-id";
+    const title = document.createElement('h4');
+    const img = document.createElement('IMG');
+    const StartPlace = document.createElement('p');
+    const startplace = 'StartPlace:  ' + Plan.StartPlace;
+    StartPlace.innerHTML = startplace;
+    const EndPlace = document.createElement('p');
+    const endplace = 'EndPlace:  ' + Plan.EndPlace;
+    EndPlace.innerHTML = endplace;
+    const MethodOfTravel = document.createElement('p');
+    const methodoftravel = 'Method of Travel:  ' + Plan.MethodOfTravel;
+    MethodOfTravel.innerHTML = methodoftravel;
+    const ExceptCost = document.createElement('p');
+    const exceptcost = 'Except Cost:  '+ Plan.ExceptCost + ' dollars';
+    ExceptCost.innerHTML = exceptcost;
+    const StartDate = document.createElement('p');
+    const startdate = 'StartDate:  ' + Plan.StartDate;
+    StartDate.innerHTML = startdate;
+    const EndDate = document.createElement('p');
+    const enddate = 'EndDate:  ' + Plan.EndDate;
+    EndDate.innerHTML = enddate;
+    const Author = document.createElement('p');
+    const author = 'Author:  ' + Plan.Author;
+    Author.innerHTML = author;
+    const NumPeople = document.createElement('pnum');
+
+    const numPeople = 'Current people ready for this trip is:  ' + Plan.numPeople;
+    NumPeople.innerHTML = numPeople;
+    NumPeople.id = Plan.PlanID + 'people';
+
+    const JoinButton = document.createElement('button');
+    JoinButton.innerHTML = 'Join the Trip';
+    JoinButton.id = Plan.PlanID;
+
+    img.src = Plan.Img;
+    img.alt = "";
+    img.className = "place";
+
+    plan.appendChild(img);
+    plan.appendChild(StartPlace);
+    plan.appendChild(EndPlace);
+    plan.appendChild(MethodOfTravel);
+    plan.appendChild(ExceptCost);
+    plan.appendChild(StartDate);
+    plan.appendChild(EndDate);
+    plan.appendChild(Author);
+    plan.appendChild(NumPeople);
+    plan.appendChild(JoinButton);
+
+    content.appendChild(plan);
+
+    document.getElementById(JoinButton.id).onclick = function(){changeNumPeople(JoinButton.id,planList)};
+
+}
+
+function changeNumPeople(ID,PlanList){
+
+    for (let i = 0; i<PlanList.length;i++){
+
+        if (PlanList[i].PlanID == ID){
+            const numID = PlanList[i].PlanID + 'people';
+            const Num = document.getElementById(numID);
+            console.log(PlanList[i].numPeople)
+            PlanList[i].numPeople+=1;
+            Num.innerHTML = 'Current people ready for this trip is:  ' + PlanList[i].numPeople;
+
+        }
+    }
+}
+
+addNewPlan(Trip1);
+function displayTrip(planList) {
+    console.log('Hello');
+    for (let i=0; i<planList.length; i++){
+        for (let j= 0; j < planList[i].length; j++){
+            if (planList[i].SpotList[j] == spot){
+                console.log(planList[i].SpotList[j])
+                addNewPlanToView(planList[i])
+            }
+        }
+        //addNewPlanToView(planList[i])
+        //console.log(disPlayList[i])
+    }
+}
+displayTrip(planList);
+
+addNewPlanToView(Trip1);
