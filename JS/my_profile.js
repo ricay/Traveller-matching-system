87aa2c613d3ce4d1d81925c9be1947027ff3e62a
ricayang <ruijia.yang@mail.utoronto.ca>
2019-12-01 00:20:15 -0500

    fix my_profile bug

--- JS/my_profile.js
+++ JS/my_profile.js
@@ -42,7 +42,12 @@ function addProfile(){
     // The data we are going to send in our request
     let data = {
         name: document.querySelector('#Username').value,
-        gender: document.querySelector('#Gender').value
+        gender: document.querySelector('#Gender').value,
+        dob: document.querySelector('#Birthday').value,
+        email: document.querySelector('#Email').value,
+        phone:document.querySelector('#Phone').value,
+        language: document.querySelector('Languages').value,
+        description:document.querySelector('Description').value,
     }
     // Create our request constructor with all the parameters we need
     const request = new Request(url, {
@@ -56,29 +61,10 @@ function addProfile(){
 
     // Send the request with fetch()
     fetch(request)
-        .then(function(res) {
-
-            // Handle response we get from the API.
-            // Usually check the error codes to see what happened.
-            const message = document.querySelector('#message')
-            if (res.status === 200) {
-                // If student was added successfully, tell the user.
-                console.log('Added student')
-                message.innerText = 'Success: Added a student.'
-                message.setAttribute("style", "color: green")
-
-            } else {
-                // If server couldn't add the student, tell the user.
-                // Here we are adding a generic message, but you could be more specific in your app.
-                message.innerText = 'Could not add student'
-                message.setAttribute("style", "color: red")
-
-            }
-            log(res)  // log the result in the console for development purposes,
-            //  users are not expected to see this.
-        }).catch((error) => {
-        log(error)
-    })
+        .then(res => {
+            window.location.href = '/dashboard'
+        })
+        .catch(error => console.log(error))
 }
 
 
