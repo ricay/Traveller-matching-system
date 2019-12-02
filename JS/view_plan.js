e808f2757c0510fb79e4ba2f0b2c5e8c6d5500c1
yangj132 <goldtrick@DESKTOP-0094F6F.localdomain>
2019-12-01 23:48:59 -0500

    change

--- JS/view_plan.js
+++ JS/view_plan.js
@@ -1,5 +1,17 @@
 var planList = [];
 var numberOfPlans = 1;
+const log = console.log;
+
+
+var planList = [];
+
+const myPlanTable = document.getElementById('myPlanList');
+var num_row = 0;
+var row;
+var index = 0;
+
+
+
 
 function jumpTo(url) {
     window.location.href = url;
@@ -30,4 +42,103 @@ function clicked(bool, id) {
         numPeople.textContent = (parseInt(numPeople.textContent) + 1).toString();
     }
     document.getElementById(id).hidden = true;
+}
+
+function loadMyPlan(){
+    const creator = 'Tony';
+    const url = '/plan';
+
+    // Since this is a GET request, simply call fetch on the URL
+    fetch(url)
+        .then((res) => {
+            if (res.status === 200) {
+                // return a promise that resolves with the JSON body
+                //log(res.json())
+;               return res.json();
+            } else {
+                alert('Could not get my plans')
+            }
+        }).then((json) => {  // the resolved promise with the JSON body
+        const plans = json.plans;
+        log(plans);
+        for (let i=0;i<plans.length;i++){
+            if (plans[i].creator == 'Tony'){
+                log('find');
+                addNewPlanToView(plans[i],index);
+                index++;
+            }
+
+        }
+
+
+    }).catch((error) => {
+        log(error)
+    })
+
+}
+
+
+
+
+function addNewPlanToView(Plan,index){
+    if (index % 4 == 0){
+        log(myPlanTable);
+        row = myPlanTable.insertRow(num_row);
+        num_row+=1;
+    }
+
+
+    const cell = row.insertCell(index % 4);
+    //const content = document.getElementById('pinBoot');
+    const plan = document.createElement('div');
+    plan.className = "white-panel";
+    plan.id = Plan.PlanID;
+
+    plan.style.background = 'white';
+    plan.style.height = 'max-content';
+    plan.style.width = '20vw';
+    plan.style.borderRadius = '5%';
+    plan.style.padding = '10px';
+
+    //const img = document.createElement('IMG');
+    const StartPlace = document.createElement('p');
+    StartPlace.innerHTML = 'StartPlace:  ' + Plan.places[0];
+    const EndPlace = document.createElement('p');
+    EndPlace.innerHTML = 'EndPlace:  ' + Plan.places[1];
+    const MethodOfTravel = document.createElement('p');
+    MethodOfTravel.innerHTML = 'Method of Travel:  ' + Plan.transportation;
+    const ExceptCost = document.createElement('p');
+    ExceptCost.innerHTML = 'Except Cost:  ' + Plan.cost + ' dollars';
+    const StartDate = document.createElement('p');
+    StartDate.innerHTML = 'StartDate:  ' + Plan.startTime;
+    const EndDate = document.createElement('p');
+    EndDate.innerHTML = 'EndDate:  ' + Plan.endTime;
+    const Author = document.createElement('p');
+    Author.innerHTML = 'Author:  ' + Plan.creator;
+
+
+
+
+
+
+    //img.src = Plan.Img;
+    //img.alt = "";
+    //img.className = "place";
+
+    //plan.appendChild(img);
+    plan.appendChild(StartPlace);
+    plan.appendChild(EndPlace);
+    plan.appendChild(MethodOfTravel);
+    plan.appendChild(ExceptCost);
+    plan.appendChild(StartDate);
+    plan.appendChild(EndDate);
+    plan.appendChild(Author);
+
+
+
+
+    cell.appendChild(plan);
+
+    //document.getElementById(JoinButton.id).onclick = function(){changeNumPeople(JoinButton.id,planList)};
+
 }
