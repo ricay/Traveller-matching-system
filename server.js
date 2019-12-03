9091a10a99898ea837eb244a515a4606a68649e5
hujianhu <billhu@LAPTOP-28H4TAC9.localdomain>
2019-12-03 09:40:39 +0800

    fix bug

--- server.js
+++ server.js
@@ -63,6 +63,15 @@ const sessionChecker = (req, res, next) => {
     }    
 };
 
+const adminSessionChecker = (req, res, next) => {
+    if (req.session.account) {
+        res.sendFile(__dirname + '/admin_main.html')
+    } else {
+        next(); // next() moves on to the route.
+    }
+};
+
+/*  route redirection  */
 app.get('/', sessionChecker, (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
@@ -84,44 +93,54 @@ app.get('/plan_trip.html', (req, res) => {
 })
 
 app.get('/create_plan.html', (req, res) => {
-    log('create_plan')
-    log(req.session)
-    log(req.sessionID)
   res.sendFile(__dirname + '/create_plan.html')
 })
 
 app.get('/user_profile.html', (req, res) => {
-    log('userProfile')
-    log(req.session)
-    log(req.sessionID)
   res.sendFile(__dirname + '/user_profile.html')
 })
 
 app.get('/my_profile.hbs', (req, res) => {
-    log('my_profile.hbs')
-    log(req.session)
-    log(req.sessionID)
   res.render('/my_profile.hbs')
 })
 
 app.get('/view_plan.html', (req, res) => {
     res.sendFile(__dirname + '/view_plan.html')
 });
+
 app.get('/tripsList.html', (req, res) => {
     res.sendFile(__dirname + '/tripsList.html')
 });
 
+app.get('/admin_login.html', adminSessionChecker, (req, res) => {
+    res.sendFile(__dirname + '/admin_login.html')
+});
 
+app.get('/admin_main.html', adminSessionChecker, (req, res) => {
+    res.sendFile(__dirname + '/admin_main.html')
+});
+
+app.get('/admin_insert_recommendation.html', adminSessionChecker, (req, res) => {
+    res.sendFile(__dirname + '/admin_insert_recommendation.html')
+});
+
+app.get('/admin_validate_users.html', adminSessionChecker, (req, res) => {
+    // log(req.session.account);
+    res.sendFile(__dirname + '/admin_validate_users.html')
+});
+
+app.get('/admin_delete_plan.html', adminSessionChecker, (req, res) => {
+    res.sendFile(__dirname + '/admin_delete_plan.html')
+});
+
+/* User signup */
 app.post('/users/signup', (req, res) => {
-    // log(1)
-    // log(req.body)
-
     const account = new Account({
         userName: req.body.userName,
         password: req.body.password,
         type: 'normal' // normal for normal users
     })
-    // log("account is " + account);
+
     account.save().then((result) => {
         const profile = new Profile({
             userName: req.body.userName,
@@ -144,18 +163,7 @@ app.post('/users/signup', (req, res) => {
     })
 });
 
-app.post('/plan/search', (req, res) => {
-    const location = req.body.location;
-    req.session.location = location;
-    res.send();
-
-});
-
-
-
-
-
-
+/* User login */
 app.post('/users/login', (req, res) => {
     const userName = req.body.userName
     const password = req.body.password
@@ -177,6 +185,19 @@ app.post('/users/login', (req, res) => {
     })
 });
 
+/* A route to logout a user*/
+app.get('/logout', (req, res) => {
+    // Remove the session
+    req.session.destroy((error) => {
+        if (error) {
+            res.status(500).send(error)
+        } else {
+            res.redirect('/')
+        }
+    })
+})
+
+/* A route to get all users*/
 app.get('/users', (req, res) => {
     Account.find().then(users => {
         res.send({ users })
@@ -185,10 +206,8 @@ app.get('/users', (req, res) => {
     })
 });
 
+// 可能是没用的
 app.get('/myprofile', sessionChecker, (req, res) => {
-    log('test')
-    log(req.session)
-    log(req.sessionID)
     Profile.findOne({userName: req.session.user}).then(user => {
         res.send({ user })
     }, (error) => {
@@ -196,10 +215,8 @@ app.get('/myprofile', sessionChecker, (req, res) => {
     })
 });
 
+// 可能是没用的
 app.get('/userProfile', sessionChecker, (req, res) => {
-    log('userProfile')
-    log(req.session)
-    log(req.sessionID)
     Profile.findOne({userName: req.session.user}).then(user => {
         log(user)
         res.send({ user })
@@ -208,6 +225,7 @@ app.get('/userProfile', sessionChecker, (req, res) => {
     })
 });
 
+/* get all profiles*/
 app.get('/profiles', (req, res) => {
     Profile.find().then((profiles) => {
         res.send({ profiles }) // can wrap in object if want to add more properties
@@ -216,18 +234,16 @@ app.get('/profiles', (req, res) => {
     })
 });
 
-
+/* get all plans*/
 app.get('/allPlan', (req, res) => {
     Plan.find().then((plans) => {
-
         res.send({ plans }) // can wrap in object if want to add more properties
     }, (error) => {
         res.status(500).send(error) // server error
     })
 });
 
-
-
+/* get all plans for the user who currently login*/
 app.get('/plan', (req, res) => {
     Plan.find({creator: req.session.userName}).then((plans) => {
 
@@ -237,11 +253,8 @@ app.get('/plan', (req, res) => {
     })
 });
 
-
+/* add a new plan */
 app.post('/plan', (req, res) => {
-    log(1);
-    log(req.body);
-
     const plan = new Plan({
         name: req.body.name,
         creator: req.session.userName,
@@ -253,27 +266,28 @@ app.post('/plan', (req, res) => {
         endTime: req.body.endTime,
         poolMember: req.body.poolMember,
     });
-    log(req.session.userName);
     plan.save().then((result) => {
         res.send(result)
     }, (error) => {
         res.status(400).send(error)
     });
-
-    log(309)
 });
 
+/* record the location that user wants to search */
+app.post('/plan/search', (req, res) => {
+    const location = req.body.location;
+    req.session.location = location;
+    res.send();
+});
 
+/* send the location that user wants to search */
 app.get('/plan/search', (req, res) => {
-    // res.send(req.session.location);
     res.send(JSON.stringify(req.session.location));
+});
 
-   });
-
-
+/* delete a plan with input id */
 app.delete('/plan/:id', (req, res) => {
     const pid = req.params.id;
-    log(pid)
     Plan.findByIdAndRemove(pid).then((student) => {
         log(student)
         if (!student) {
@@ -286,7 +300,7 @@ app.delete('/plan/:id', (req, res) => {
     })
 });
 
-
+/* get all places */
 app.get('/allPlace', (req, res) => {
     Plan.find({creator: req.session.userName}).then((plans) => {
 
@@ -296,10 +310,131 @@ app.get('/allPlace', (req, res) => {
     })
 });
 
-// a user want to join other user's plan
+/* a user want to join other user's plan */
 app.patch('/plan/:pid', (req, res) => {
     const currUserName = req.session.userName
 })
+
+/* admin routes */
+app.post('/index/onload', (req, res) => {
+    const newAdmin = new Account({
+        userName: req.body.userName,
+        password: req.body.password,
+        type: req.body.type
+    });
+    newAdmin.save().then((result) => {
+        res.send(result);
+    }, (error) => {
+        res.status(400).send(error);
+    })
+});
+
+/*get profile*/
+app.get('/getProfile',(req,res) =>{
+    Profile.findOne({userName:req.session.userName}).then((profile) => {
+        console.log(req.session.userName);
+        console.log(profile);
+        res.send({ profile })
+    },(error) => {
+        res.status(500).send(error)
+    })
+});
+
+//a route to user_profile
+app.put('/editProfile', (req, res) => {
+    const userName = req.body.userName;
+    const first = req.body.firstName;
+    const last = req.body.lastName;
+    const gender = req.body.gender;
+    const dob = req.body.birthday;
+    const email = req.body.email;
+    const phone = req.body.phone;
+    // const language = req.body.language;
+    const description = req.body.description;
+    log(req.session.userName);
+
+    //save to database
+    Profile.findOne({userName:req.session.userName}).then(profile => {
+        log(profile);
+        if (!profile) {
+            res.status(404).send()
+        } else {
+            profile.userName = userName;
+            profile.firstName = first;
+            profile.lastName = last;
+            profile.gender = gender;
+            profile.birthday = dob;
+            profile.email = email;
+            profile.phone = phone;
+            // profile.language = language;
+            profile.description = description;
+
+            profile.save().then((result) => {
+                res.send({result});
+            }, (error) => {
+                res.status(404).send(error)
+            });
+        }
+    }).catch((error) => {
+        log(2);
+        res.status(500).send()
+    });
+});
+
+app.post('/admin/login/:userName/:password', (req, res) => {
+    const userName = req.params.userName;
+    const password = req.params.password;
+
+    Account.findByUserNamePassword(userName, password).then((account) => {
+        if (!account) {
+            res.status(404).send()
+        } else {
+            // req.session.account = account._id;
+            // log(req.session.account);
+            res.status(200).send()
+        }
+    }).catch((error) => {
+        res.status(400).send(error)
+    })
+});
+
+
+app.get('/admin/getUsers', (req, res) => {
+    Profile.find().then((profiles) => {
+        res.send({ profiles }) // can wrap in object if want to add more properties
+    }, (error) => {
+        res.status(500).send(error) // server error
+    })
+});
+
+app.delete('/admin/deleteUser/:userName', (req, res) => {
+    const userName = req.params.userName;
+    // Delete account
+    Account.deleteByUserName(userName).then((account) => {
+        log(account);
+        if (!account) {
+            res.status(404).send()
+        }
+        else {
+            res.status(200).send()
+        }
+    }).catch((error) => {
+        res.status(500).send()
+    });
+
+    // Delete profile
+    Profile.deleteByUserName(userName).then((profile) => {
+        log(profile);
+        if (!profile) {
+            res.status(404).send()
+        }
+        else {
+            res.status(200).send()
+        }
+    }).catch((error) => {
+        res.status(500).send()
+    })
+});
 
 
 /*************************************************/
