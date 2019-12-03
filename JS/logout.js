c277e4a590f4e70668e7a8fef8d60d33a9e91f08
hujianhu <billhu@LAPTOP-28H4TAC9.localdomain>
2019-12-03 23:49:37 +0800

    fix

--- JS/logout.js
+++ JS/logout.js
@@ -13,3 +13,27 @@ function adminLogout() {
         }
     });
 }
+
+function userLogout() {
+    const url = '/logout';
+    fetch(url)
+        .then((res) => {
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "You will go back to home page.",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes'
+            }).then((result) => {
+                if (result.value) {
+                    window.location.href = 'index.html';
+                }
+            });        
+
+        }).catch((error) => {
+            log(error)
+        })
+    
+}
